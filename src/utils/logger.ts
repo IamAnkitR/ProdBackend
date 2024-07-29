7deac7c55e1f02ae1c2309750ed304410b256485
IamAnkitR <ardb8755@gmail.com>
2024-07-29 15:19:53 +0530

    fix: log file removed

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -10,10 +10,7 @@ const logFormat = winston.format.combine(
 const logger = winston.createLogger({
   level: 'info',
   format: logFormat,
-  transports: [
-    new winston.transports.File({ filename: 'logs/app.log' }),
-    new winston.transports.Console(),
-  ],
+  transports: [new winston.transports.Console()],
 });
 
 export default logger;
