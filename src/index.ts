aebcecb0d9480ac25114293bfd2ce1029885245b
IamAnkitR <ardb8755@gmail.com>
2024-07-29 14:53:37 +0530

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv, { config } from "dotenv";
+import app from "./app";
+import { PORT, DATABASE_URL } from "./config";
+
+dotenv.config();
+
+console.log(DATABASE_URL);
+
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => {
+    console.log(`Connected to MongoDB Database`);
+
+    app.listen(PORT, () => {
+      console.log(`Server started at port ${PORT}`);
+    });
+  })
+  .catch((error: any) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
